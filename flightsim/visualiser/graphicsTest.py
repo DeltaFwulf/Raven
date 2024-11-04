1433041aaf966536e3dcbbef20a456c566eb578d
DeltaV <58670635+DeltaFwulf@users.noreply.github.com>
2024-11-04 00:20:49 +0000

    Update graphicsTest.py

--- flightsim/visualiser/graphicsTest.py
+++ flightsim/visualiser/graphicsTest.py
@@ -16,6 +16,20 @@ from math import cos, sin, pi
 # currently, it is a 1x1 right cylinder:
 # for future reference, this will be run at initialisation of each primitive, then stored in each object as tuples
 def getConicVertices(height:float, r0_outer:float, r0_inner:float, r1_outer:float, r1_inner:float, curvFaces:int):
+    """
+    Vertex order: outer ring 0, outer ring 1, inner ring 0, inner ring 1
+    
+    
+    """
+
+    x0 = 0
+    xf = x0 + height
+
+    # prevent negative values
+    r0_inner = max(0, r0_inner)
+    r1_inner = max(0, r1_inner)
+    r0_outer = max(0, r0_outer)
+    r1_outer = max(0, r1_outer)
 
     v0_outer = []
     v0_inner = []
@@ -23,16 +37,22 @@ def getConicVertices(height:float, r0_outer:float, r0_inner:float, r1_outer:floa
     v1_outer = []
     v1_inner = []
 
+    offset_inner0 = 2 * curvFaces
+    offset_inner1 = 3 * curvFaces if r0_inner > 0 else 2 * curvFaces + 1
+
+    edges = []
+
     ang = np.linspace(0, 2 * pi, curvFaces)
 
     # create outer conic vertices
     for i in range(0, curvFaces):
 
-        v0_outer.append([r0_outer * cos(ang[i]), r0_outer * sin(ang[i]), 0])
-        v1_outer.append([r1_outer * cos(ang[i]), r1_outer * sin(ang[i]), height])
+        v0_outer.append((r0_outer * cos(ang[i]), r0_outer * sin(ang[i]), x0))
+        v1_outer.append((r1_outer * cos(ang[i]), r1_outer * sin(ang[i]), xf))
 
-    v0_outer = tuple(v0_outer)
-    v1_outer = tuple(v1_outer)
+        edges.append((i, i+1)) # ring 0
+        edges.append((i + curvFaces, i + curvFaces + 1)) # ring 1
+        edges.append((i, i + curvFaces)) # ring 0 to ring 1
 
     # we have 4 configurations of the inner surface:
     # 1): there is no inner hole (inners both equal 0) (no inner vertices)
@@ -40,20 +60,41 @@ def getConicVertices(height:float, r0_outer:float, r0_inner:float, r1_outer:floa
     # 3): upper is 0, lower is non-zero (many to point)
     # 4): both are non-zero (many to many)
 
-    if(r0_inner > 0):
+    if(r0_inner > 0 and r1_inner > 0):
+        for i in range(0, curvFaces):
+            
+            v0_inner.append((r0_inner * cos(ang[i]), r0_inner * sin(ang[i]), x0))
+            v1_inner.append((r1_inner * cos(ang[i]), r1_inner * sin(ang[i]), xf))
 
-        if(r1_inner > 0):
-            for i in range(0, curvFaces):
-                v0_inner.append([r0_inner * cos(ang[i]), r0_inner * sin(ang[i]), 0])
-                v1_inner.append([r1_inner * cos(ang[i]), r1_inner * sin(ang[i]), height])
+            edges.append((i + offset_inner0, i + offset_inner0 + 1)) # inner ring 0
+            edges.append((i + offset_inner1, i + offset_inner1 + 1)) # inner ring 1
+            edges.append((i + offset_inner0, i + offset_inner1)) # inner ring 0 -> inner ring 1
+            
+
+    elif(r0_inner == 0 and r1_inner > 0):
+        
+        v0_inner.append([0, 0, x0])
+
+        for i in range(0, curvFaces):
+            v1_inner.append((r1_inner * cos(ang[i]), r1_inner * sin(ang[i]), xf))
+            edges.append((offset_inner0, i + offset_inner1))
+
+    elif(r0_inner > 0 and r1_inner == 0):
+
+        v1_inner.append([0, 0, xf])
+
+        for i in range(0, curvFaces):
+            v0_inner.append((r0_inner * cos(ang[i]), r0_inner * sin(ang[i]), x0))
+            edges.append((i + offset_inner0, offset_inner1))
+
+    vertices = (v0_outer, v1_outer, v0_inner, v1_inner)
+    edges = tuple(edges)
+
+    return vertices, edges
 
 
-
-
-
-
-
-# cube vertices:
+"""
+#cube vertices:
 vertices = ((1, -1, -1),
             (1, 1, -1),
             (-1, 1, -1),
@@ -75,9 +116,12 @@ edges = ((0,1),
          (5,1),
          (5,4),
          (5,7))
+"""
 
 
-def Cube():
+
+
+def shape(vertices:tuple, edges:tuple):
 
     glBegin(GL_LINES)
     
@@ -89,6 +133,11 @@ def Cube():
 
 
 def main():
+
+    vertices, edges = getConicVertices(height=1, r0_outer=1, r0_inner=0.5, r1_outer=1, r1_inner=0.5, curvFaces=8)
+
+    print(vertices)
+    print(edges)
 
     pg.init()
     display = (800, 600)
@@ -106,9 +155,10 @@ def main():
 
         glRotate(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        Cube()
+        shape(vertices, edges)
         pg.display.flip() # updates the display
         pg.time.wait(10)
+
 
 
 main()
